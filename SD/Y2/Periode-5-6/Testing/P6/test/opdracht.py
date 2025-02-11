46a2fa8df61785154500e8e3aa5b0082c1e6ce87
Sufiyaan <145906231+MSHS-25@users.noreply.github.com>
2025-02-11 09:37:21 +0100

    aanpassingen

--- SD/Y2/Periode-5-6/Testing/P6/test/opdracht.py
+++
@@ -1,9 +0,0 @@
-def authenticeer_gebruiker(gebruikersnaam, wachtwoord):
-    gebruikers = {
-        "admin_vdb": "Adminwachtwoord",
-        "abdel_020": "wachtwoord123",
-        "piet_69": "sixnine",
-        "motje_vdb": "TurkijeNummerEen"
-        
-    }
-    return gebruikers.get(gebruikersnaam) == wachtwoord
--- SD/Y2/Periode-5-6/Testing/P6/week-2-py/src/opdracht.py
+++
@@ -1,7 +0,0 @@
-def find_max_value(numbers):
-    max_value = 0;
-
-    for number in numbers:
-        if number > max_value: # < is fout
-            max_value = number
-    return max_value;
